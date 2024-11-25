c8654d7b299a3a3cce8c0786e9b00ad5a34971cb
Muhammed Rasim <muhammedrasim26@gmail.com>
2024-11-25 12:54:40 +0530

    Updates to be added

---
+++ lib/presentation/main_page/widgets/screen_main_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_clone/presentation/WishList/wish_list.dart';
+import 'package:netflix_clone/presentation/downloads/widgets/screen_downloads.dart';
+import 'package:netflix_clone/presentation/home/screen_home.dart';
+import 'package:netflix_clone/presentation/main_page/widgets/bottom_navigation.dart';
+import 'package:netflix_clone/presentation/search/search.dart';
+
+// This is the main page.
+class ScreenMainPage extends StatelessWidget {
+  ScreenMainPage({super.key});
+
+  final _pages = [
+    ScreenHome(),
+    SearchScreen(),
+    ScreenWishList(),
+    ScreenDownloads(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ValueListenableBuilder(
+          valueListenable: indexChangeNotifier,
+          builder: (context, int index, _) {
+            return _pages[index];
+          },
+        ),
+      ),
+      bottomNavigationBar: const BottomNavigationWidget(),
+    );
+    //ui il ulla athe order il thanne code ilum
+    //kondu varan shramikkuka.
+  }
+}
